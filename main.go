ec598640c63764cf2d5005a438280592824f2d7d
minjoo0729 <minjoo0729@kaist.ac.kr>
2024-03-07 12:10:06 +0900

    feat: Go Routine

--- main.go
+++ main.go
@@ -1,48 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("request failed")
-
-func main() {
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcodes.co/",
-	}
-
-	// var results = map[string]string{}
-	var results = make(map[string]string)
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, status := range results {
-		fmt.Printf("Check : %s\tStatus : %s\n", url, status)
-	}
+func main(){
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
-	fmt.Printf("Checking %s\n", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err)
-		return errRequestFailed
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
